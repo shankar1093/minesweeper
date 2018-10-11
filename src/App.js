3cd4a0faa715a860cd6b29e8a57f14d06337252d
Shankar Rao <shankar1093@gmail.com>
2018-10-10 22:48:42 -0400

    Updated App.js file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import Board from './component/Board';
+import './style.scss';
 
-class App extends Component {
+class Game extends React.Component {
+  state = {
+    height: 8,
+    width: 8,
+    mines: 10   
+  };
+
   render() {
+    const { height, width, mines } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="game">
+        <Board height={height} width={width} mines={mines} />
       </div>
     );
   }
 }
 
-export default App;
+ReactDOM.render(<Game />, document.getElementById("root"));
+
